b9e0f78245bc77bef160567c03220ff5af47d16b
Karalius <33183813+Karalius@users.noreply.github.com>
2021-05-09 13:23:23 +0300

    Update heroku_credentials.py

--- scrape_etsy/heroku_credentials.py
+++ scrape_etsy/heroku_credentials.py
@@ -1,8 +1,8 @@
 # Heroku Postgres credentials, define here:
 
-DATABASE = "dfd5ufck729gtm"
-USER = "gkyhzpkfcptiql"
-PASSWORD = "3738f7107f298b27a52a6c3413971c50202d4c9fdea2dfdcd3b8f06c663aac05"
-HOST = "ec2-54-220-35-19.eu-west-1.compute.amazonaws.com"
-PORT = "5432"
-POSTGRES_URI = 'postgresql://gkyhzpkfcptiql:3738f7107f298b27a52a6c3413971c50202d4c9fdea2dfdcd3b8f06c663aac05@ec2-54-220-35-19.eu-west-1.compute.amazonaws.com:5432/dfd5ufck729gtm'
+DATABASE = ""
+USER = ""
+PASSWORD = ""
+HOST = ""
+PORT = ""
+POSTGRES_URI = ''
